621b6a0e6b88ed4a124a49b5527710c9af4d94bf
letsila <t.razafinirina@gmail.com>
2017-05-21 16:03:39 +0300

    First commit

---
+++ App/Network/api.js
@@ -0,0 +1,4 @@
+var API_ENDPOINT = '/res'
+
+exports.PRODUCTS = API_ENDPOINT + '/products.json';
+exports.CATEGORIES = API_ENDPOINT + '/categories.json';
---
+++ index.ios.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ListView
+} from 'react-native';
+
+var products = require("./res/json/products.json");
+
+export default class POSReactNative extends Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(products),
+    }
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>{rowData.name}</Text>}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('POSReactNative', () => POSReactNative);
